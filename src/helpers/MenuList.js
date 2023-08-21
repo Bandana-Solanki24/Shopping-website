ec30f83d20c766a0012dc21e06eeee62bb626b86
Bandana-Solanki24 <solankibandana24@gmail.com>
2023-08-21 12:47:51 +0530

    Fix line ending issues

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 215.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 411.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 317.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 234.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 997.99,
+  },
+];
